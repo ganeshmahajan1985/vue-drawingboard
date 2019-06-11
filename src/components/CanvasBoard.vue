7875457ab88f3625c18cb5f1c4037ee05acae288
Ganesh Prakash Mahajan <ganeshmahajan@qnap.com>
2019-06-11 21:38:28 +0800

    updated the lint errors and width of canvas

--- src/components/CanvasBoard.vue
+++ src/components/CanvasBoard.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="canvasContainer">
-    <div class="canvasContainer_layout">
-      <div class="canvascontainer_buttons">
-        <div class="canvascontainer_buttons__pencileraser">
-          <button @click="Pencil">Pencil</button>
-          <button @click="Eraser">Eraser</button>
+  <div class='canvasContainer'>
+    <div class='canvasContainer_layout'>
+      <div class='canvascontainer_buttons'>
+        <div class='canvascontainer_buttons__pencileraser'>
+          <button @click='Pencil'>Pencil</button>
+          <button @click='Eraser'>Eraser</button>
         </div>
-        <div class="canvascontainer_buttons__redoundo">
-          <button @click="Undo">Undo</button>
-          <button @click="Redo">Redo</button>
+        <div class='canvascontainer_buttons__redoundo'>
+          <button @click='Undo'>Undo</button>
+          <button @click='Redo'>Redo</button>
         </div>
       </div>
       <canvas
-        class="contextCanvas"
-        width="700"
-        height="500"
-        ref="canvas"
-        id="drawing-pad"
+        class='contextCanvas'
+        width='700'
+        height='500'
+        ref='canvas'
+        id='drawing-pad'
       >This is a drawing pad for Incubit</canvas>
     </div>
   </div>
@@ -30,16 +30,16 @@ export default {
       isDrawing: false,
       startX: 0,
       startY: 0,
-      points: []
+      points: [],
     };
   },
   mounted() {
     const vm = this;
     vm.canvas = vm.$refs.canvas;
-    vm.context = vm.canvas.getContext("2d");
-    vm.canvas.addEventListener("mousedown", vm.mousedown);
-    vm.canvas.addEventListener("mousemove", vm.mousemove);
-    document.addEventListener("mouseup", vm.mouseup);
+    vm.context = vm.canvas.getContext('2d');
+    vm.canvas.addEventListener('mousedown', vm.mousedown);
+    vm.canvas.addEventListener('mousemove', vm.mousemove);
+    document.addEventListener('mouseup', vm.mouseup);
   },
   methods: {
     mousedown(e) {
@@ -52,7 +52,7 @@ export default {
       vm.startY = y;
       vm.points.push({
         x,
-        y
+        y,
       });
     },
     mousemove(e) {
@@ -65,14 +65,16 @@ export default {
         vm.context.moveTo(vm.startX, vm.startY);
         vm.context.lineTo(x, y);
         vm.context.lineWidth = 1;
-        vm.context.lineCap = "round";
-        vm.context.strokeStyle = "rgba(0,0,0,1)";
+        vm.context.lineCap = 'round';
+        vm.context.lineJoin = 'round';
+        vm.context.lineWidth = 3;
+        vm.context.strokeStyle = 'rgba(0,0,0,1)';
         vm.context.stroke();
         vm.startX = x;
         vm.startY = y;
         vm.points.push({
           x,
-          y
+          y,
         });
       }
     },
@@ -80,7 +82,7 @@ export default {
       const vm = this;
       vm.isDrawing = false;
       if (vm.points.length > 0) {
-        localStorage.setItem("points", JSON.stringify(vm.points));
+        localStorage.setItem('points', JSON.stringify(vm.points));
       }
     },
     Pencil() {
@@ -94,11 +96,11 @@ export default {
     },
     Redo() {
       return true;
-    }
-  }
+    },
+  },
 };
 </script>
-<style scoped lang="scss" >
+<style scoped lang='scss' >
 .canvasContainer {
   text-align: center;
   display: flex;
